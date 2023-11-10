a76d9f3b3c8bf6dbf7bdc5d7cfc4243f612953e0
kantharao <julurukantharao@gmail.com>
2023-11-10 12:03:55 -0500

    replaced new line char

--- appDeployments/WID5/deploy-app.sh
+++ appDeployments/WID5/deploy-app.sh
@@ -80,13 +80,13 @@ kubectl config set-context --current --namespace="$APP_NAMESPACE"
 
 kubectl delete configmap conjur-connect --ignore-not-found=true
 
-kubectl create configmap conjur-connect `
-  --from-literal CONJUR_ACCOUNT="$CONJUR_ACCOUNT" `
-  --from-literal CONJUR_APPLIANCE_URL="$CYBERARK_CONJUR_APPLIANCE_URL" `
-  --from-literal CONJUR_AUTHN_URL="$CYBERARK_CONJUR_APPLIANCE_URL"/authn-jwt/"$CONJUR_AUTHENTICATOR_ID" `
-  --from-literal AUTHENTICATOR_ID="$CONJUR_AUTHENTICATOR_ID" `
-  --from-literal CONJUR_AUTHN_JWT_SERVICE_ID="$CONJUR_AUTHENTICATOR_ID" `
-  --from-literal MY_POD_NAMESPACE="$APP_NAMESPACE" `
+kubectl create configmap conjur-connect \
+  --from-literal CONJUR_ACCOUNT="$CONJUR_ACCOUNT" \
+  --from-literal CONJUR_APPLIANCE_URL="$CYBERARK_CONJUR_APPLIANCE_URL" \
+  --from-literal CONJUR_AUTHN_URL="$CYBERARK_CONJUR_APPLIANCE_URL"/authn-jwt/"$CONJUR_AUTHENTICATOR_ID" \
+  --from-literal AUTHENTICATOR_ID="$CONJUR_AUTHENTICATOR_ID" \
+  --from-literal CONJUR_AUTHN_JWT_SERVICE_ID="$CONJUR_AUTHENTICATOR_ID" \
+  --from-literal MY_POD_NAMESPACE="$APP_NAMESPACE" \
   --from-file CONJUR_SSL_CERTIFICATE="$CONJUR_CERT_FILE"
 
 kubectl delete serviceaccount demo-app-sa --ignore-not-found=true
